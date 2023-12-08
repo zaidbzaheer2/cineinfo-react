a84db83cd2ecf1731d5617b66670583f36fe3b8b
zaidbzaheer2 <zaidbzaheer2@gmail.com>
2023-12-08 18:39:33 +0500

    Static Files

--- components/NotFound/NotFound.jsx
+++ components/NotFound/NotFound.jsx
@@ -1,7 +1,8 @@
+import NotFoundIMG from "../../src/assets/static/NotFound.jpg"
 export const NotFound  = () =>{
     return <section className="404-section ">
         <div className="error-container flex flex-col items-center w-full">
-            <img src="../../../static/404img.jpg"/>
+            <img src={NotFoundIMG}/>
         </div>
     </section>
 }
