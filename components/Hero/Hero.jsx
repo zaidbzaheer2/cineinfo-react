254a3b0f14085a2afe249e6e1d73c67f9ff8abef
zaidbzaheer2 <zaidbzaheer2@gmail.com>
2023-12-08 22:10:38 +0500

    Improved UI

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ export const Hero = (props) =>{
     const {dark, setDark}= props
     return (
         <section className={"hero-section flex absolute top-[5rem] flex-col justify-center dark:bg-neutral-950 items-center w-full  h-full " + (dark? "black-scroll":"white-scroll")}>
-            <div className="hero-container  w-[90%] md:w-[80%]  mt-[3rem] h-screen">
+            <div className="hero-container  w-[90%] md:w-[80%] mt-[8rem] md:mt-[3rem]  h-screen">
                 <AllRoutes/>
             </div>
         </section>
